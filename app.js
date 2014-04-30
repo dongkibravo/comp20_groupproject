c6b3eda06d78f842f48ed8c12d1c386e6ee7b2d7
dong <dongkibravo@hotmail.com>
2014-04-30 13:23:24 -0400

    asd
    asdv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongo = require('mongodb');
+var name; 
  
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.static(__dirname +'/public'));
+app.listen(process.env.PORT || 3000);
+
 
 var mongoUri = process.env.MONGOLAB_URI ||
 	process.env.MONGOHQ_URL ||
@@ -25,28 +28,38 @@ app.all('*', function(req, res, next) {
 });
 
 app.get('/', function(req, res){
+
 	res.render('index.html');
+	
 });
 
-/*
+
+
 app.post('/submit.json', function(req, res){
 
 
 //posting portal information
 	db.collection('portal-information', function(error, collection) {
-		collection.insert({"order":request.body.username, 
-						   "score":parseInt(request.body.score), 
-						   "grid":request.body.grid, "created_at":request.body.created_at},
-						  function(error, saved) {
-			response.send(200);
+		//collection.insert({_id:req.body.username, number:req.body.number, users:[]}, function(error, saved){
+				collection.insert({_id:"dong", number:1234, users:[]}, function(error, saved){
+
+			res.send(200);
 		});
 	});
+	
+	
+});
 
 
 
+app.get('/data.json', function(req, res){
+
+	db.collection('portal-information', function(error, collection){
+		collection.find().toArray(function(error, docs){
+			res.send(JSON.stringify(docs));
+		});
+	});
+	
+});
 
 
-}*/
-
-
-app.listen(process.env.PORT || 3000);
